054d785f0944bfc37c0b66d1884a81421e8b7e5a
Dan Jackson <idontwantyourjunk29@gmail.com>
2019-06-11 17:30:56 -0400

    Changed a Thing

--- driver.py
+++ driver.py
@@ -7,7 +7,6 @@ voiceEngine.setProperty(
     "voice",
     r"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_ZIRA_11.0",
 )
-print(5)
 
 while True:
     voiceEngine.say("What can I help you with today?")
